2dbf76367d8ad844451d8d42ce78a8b6af09e466
rrsq <ranhua@live.cn>
2022-04-11 16:56:49 +0800

    必填项202204-11-02

--- src/main/scala/rdd/test3.scala
+++ src/main/scala/rdd/test3.scala
@@ -4,5 +4,6 @@ object test3{
   def main(args: Array[String]): Unit = {
     println("这不好hao are you ！")
     println("today is 2022/04/11,come on!")
+    println("today is 2022/04/11-2222,come on!")
   }
 }
